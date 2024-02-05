ff8e1b5f46524de17df75f40d79ebdfcc460bc8c
ArwaYearub <153165419+ArwaYearub@users.noreply.github.com>
2024-02-05 23:10:14 +0300

    atsk 3 commit
    add string var (greeting) 
    console.log (greeting + name) with a space in between for readability

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var name = 'arwa';
+var name = 'Arwa';
 var age = 20;
 var isStudent = true;  // arwa(me) assumed this is a true boolean.
 console.log('name', name);
@@ -6,4 +6,8 @@ console.log('age', age);
 console.log('isStudent', isStudent);
 
 var variableToCheck = 100; // arwa(me) declared this variable to be a number.
-console.log('the data type of the variable "variableToCheck" is ', typeof variableToCheck)
+console.log('the data type of the variable "variableToCheck" is ', typeof variableToCheck);
+
+var greeting = 'Welcome';
+// string name = 'arwa'; //since the var name is alrady declared, there is no need to declare it again.
+console.log(greeting + ' ' + name) //when checked the name and greeting were stuch to each other therefore i added a space between them.
