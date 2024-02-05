812dfd6c4d61e8edb4c1bf4b930a9603086aee15
ArwaYearub <153165419+ArwaYearub@users.noreply.github.com>
2024-02-05 22:58:31 +0300

    console.log the 3 variables

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
 var name = 'arwa';
 var age = 20;
-var isStudent = Boolean;
+var isStudent = true;  // arwa(me) assumed this is a true boolean.
+console.log('name', name);
+console.log('age', age);
+console.log('isStudent', isStudent);
