00ac87adb7038abf34bb6587b8480ef40a52864f
ArwaYearub <153165419+ArwaYearub@users.noreply.github.com>
2024-02-05 23:03:37 +0300

    task 2, varaibleToCheck added
    all 3 steps

--- index.js
+++ index.js
@@ -4,3 +4,6 @@ var isStudent = true;  // arwa(me) assumed this is a true boolean.
 console.log('name', name);
 console.log('age', age);
 console.log('isStudent', isStudent);
+
+var variableToCheck = 100; // arwa(me) declared this variable to be a number.
+console.log('the data type of the variable "variableToCheck" is ', typeof variableToCheck)
